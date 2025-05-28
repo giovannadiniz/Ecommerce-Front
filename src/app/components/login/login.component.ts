e2eb9c76ea816f45ac01858a467a6fbeb14c87f7
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2025-05-27 23:37:10 -0300

    feature: login integrado com backend com sucesso.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
+import { LoginRequest } from '../../interfaces/LoginRequest';
+import { LoginResponse } from '../../interfaces/LoginResponse';
 
 
 @Component({
@@ -12,17 +15,35 @@ import { Router, RouterModule } from '@angular/router';
   styleUrls: ['/login.component.scss']
 })
 export class LoginComponent {
-  email: string = '';
+  username: string = '';
   password: string = '';
+  errorMessage: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthenticationService) {}
 
   onSubmit() {
-    console.log('Login attempt with:', { email: this.email, password: this.password });
-    // Aqui você adicionaria a lógica de autenticação
+    const loginData: LoginRequest = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.authService.loginUser(loginData).subscribe({
+      next: (response: LoginResponse) => {
+        console.log('Login bem-sucedido:', response);
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+        }
+        this.router.navigate(['']);
+      },
+      error: (error) => {
+        console.error('Erro no login:', error);
+        this.errorMessage = 'Usuário ou senha inválidos. Tente novamente.';
+      }
+    });
   }
 
   goRegister(){
     this.router.navigate(['/register']);
   }
+  
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,16 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../../domain/user';
 import { Observable } from 'rxjs';
+import { LoginResponse } from '../interfaces/LoginResponse';
+import { LoginRequest } from '../interfaces/LoginRequest';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private readonly apiUrl = 'http://localhost:8080/ecommerce/auth'; // Ajuste a URL conforme necessário
+  private readonly apiUrl = 'http://localhost:8080/ecommerce/auth'; 
 
     constructor(private http: HttpClient) { }
 
       registerUser(userData: User): Observable<User> {
        return this.http.post<User>(`${this.apiUrl}/register`, userData);
   }
+      loginUser(loginData: LoginRequest): Observable<LoginResponse> {
+      return this.http.post<LoginResponse>(`${this.apiUrl}/login`, loginData);
+      }
 }
