95b904a35beabc3dca495351f82d9a86afc9bc48
Giovanna Diniz <giovanna.diniz.56@gmail.com>
2025-06-23 21:43:50 -0300

    feature: navbar e footer componentizados e implementados

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {AuthenticationService} from '../../services/authentication.service';
+import {CartComponent} from '../cart/cart.component';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  imports: [
+    CartComponent
+  ],
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  isLoggedIn = false;
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {
+    // Verificar o status de login
+    // this.authService.isLoggedIn$.subscribe(loggedIn => {
+    //   this.isLoggedIn = loggedIn;
+    // });
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
